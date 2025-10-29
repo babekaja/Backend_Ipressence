eb69a47bc62993b1700853dfdb00bf4b4acee45a
barnabe bugugu <bugugubarnabe33@gmail.com>
2025-10-30 01:52:40 +0200

    Add specific CORS origin for ipressence-web2

--- api.php
+++ api.php
@@ -15,6 +15,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: https://ipressence-web2.vercel.app');
 
 // Gestion de la requête OPTIONS (Preflight pour CORS)
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
